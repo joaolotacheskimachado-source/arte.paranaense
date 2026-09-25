770d68fb6e8e65731a47b639aa7a19266d06a787
joaolotacheskimachado-source <joao.lotacheski.machado@escola.pr.gov.br>
2026-09-25 08:24:29 -0300

    atualizei o index

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-
+const botoesCurtir = document.queriSelectorAll (".curtir");
+botoesCurtir.forEach(function(botaoCurtir){
+    let curtiu=false;
+    botaoCurtir.addEventLister("click", curtir);
+function curtir(){
+    const contador= botaoCurtir.querySelector("span");
+    if(curtir=== false){
+        contador.-textContent++;
+        curtir = true;
+    }
+    else{
+        contador.textContent--;
+        curtir= false;
+    }
+    
+}
+});
+    
+     
